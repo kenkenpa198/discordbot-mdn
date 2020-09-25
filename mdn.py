9d768a8abe264bb30244cf052274880b78f9ce56
kenkenpa198 <kenkenpa198@gmail.com>
2020-09-25 20:58:11 +0900

    [wip]

--- mdn.py
+++ mdn.py
@@ -6,8 +6,8 @@ import config
 print('===== もだねちゃん起動 =====')
 print('discord.py ' + discord.__version__)
 
-# bot = commands.Bot(command_prefix='!mdn ') # コマンド実行を示す「!mdn 」を指定
-bot = commands.Bot(command_prefix='?mdn ') # 開発用
+bot = commands.Bot(command_prefix='!mdn ') # コマンド実行を示す「!mdn 」を指定
+# bot = commands.Bot(command_prefix='?mdn ') # 開発用
 
 # 起動時に動作する処理
 @bot.event
