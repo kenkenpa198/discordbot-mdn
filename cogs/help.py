cd7e787aedb76ab07af336a0e3df6be12e2c43a9
kenkenpa198 <kenkenpa198@gmail.com>
2020-12-06 13:13:53 +0900

    [update]ヘルプ一覧を調整

--- cogs/help.py
+++ cogs/help.py
@@ -12,17 +12,23 @@ class Help(commands.Cog):
         print('===== もだねちゃんヘルプを表示します =====')
         embed = discord.Embed(title='もだねちゃんヘルプ', description='もだねちゃんのお仕事コマンド一覧だよ！\nもっと詳しい操作方法は[ 📙 ガイドブック ](https://github.com/kenkenpa198/discordbot-mdn/blob/master/README.md)を確認してみてね！', color=0xffd6e9)
 
-        help_talk = f'ㅤ\n\n■ 読み上げコマンド'
-        mdn_s = f'ㅤ\n🎤 読み上げを開始する```!mdn s```'
-        mdn_c = f'ㅤ\n🎤 読み上げ対象のテキストチャンネルを再設定する```!mdn c```'
-        mdn_e = f'ㅤ\n🎤 読み上げを終了する```!mdn e```'
-        embed.add_field(name=help_talk, value=mdn_s + mdn_c + mdn_e, inline=False)
+        help_name    = f'ㅤ\n🎤 読み上げ機能'
+        help_value_1 = f'ㅤ- 読み上げを開始する```!mdn s```'
+        help_value_2 = f'ㅤ\nㅤ- 読み上げ対象のテキストチャンネルを再設定する```!mdn c```'
+        help_value_3 = f'ㅤ\nㅤ- 読み上げを終了する```!mdn e```'
+        embed.add_field(name=help_name, value=help_value_1 + help_value_2 + help_value_3, inline=False)
 
-        help_ohter = f'ㅤ\n■ その他'
-        mdn_j = f'ㅤ\n✌️ ジャンケンで遊ぶ```!mdn j```'
-        mdn_u = f'ㅤ\n🔮 もだねちゃん占い```!mdn u```'
-        mdn_h = f'ㅤ\n❓ ヘルプを表示する```!mdn h```'
-        embed.add_field(name=help_ohter, value=mdn_j + mdn_u + mdn_h, inline=False)
+        help_name    = f'ㅤ\n✌️ジャンケンで遊ぶ'
+        help_value_1 = f'```!mdn j```'
+        embed.add_field(name=help_name, value=help_value_1, inline=False)
+
+        help_name    = f'ㅤ\n🔮 もだねちゃん占い'
+        help_value_1 = f'```!mdn u```'
+        embed.add_field(name=help_name, value=help_value_1, inline=False)
+
+        help_name    = f'ㅤ\n❓ ヘルプを表示する'
+        help_value_1 = f'```!mdn h```'
+        embed.add_field(name=help_name, value=help_value_1, inline=False)
 
         await ctx.send(embed=embed)
 
