f823e1cbc12bb3ed667fb8ae745ad9d96846d9ee
kenkenpa198 <kenkenpa198@gmail.com>
2022-11-08 20:46:06 +0900

    [update]リストの読込処理をコンストラクタ内で実行するように変更

--- discordbot-mdn/cogs/uranai.py
+++ discordbot-mdn/cogs/uranai.py
@@ -10,25 +10,22 @@ from discord.ext import tasks
 from .utils import psql
 from .utils import send as sd
 
-##### 占い用リストの読み込み #####
-# 運勢リスト
-with open('./csv/fortune_list.csv', 'r', encoding='utf-8') as f:
-    fortune_list = [s.strip() for s in f.readlines()]
-# print(fortune_list)
-
-# 運勢の星リスト
-star_list = ['★', '★★', '★★★', '★★★★', '★★★★★', '⭐️⭐️⭐️⭐️⭐️⭐️']
-
-# ラッキーアイテムリスト
-with open('./csv/lucky_list.csv', 'r', encoding='utf-8') as f:
-    lucky_list = [s.strip() for s in f.readlines()]
-# print(lucky_list)
-
-
-##### コグ #####
 class Uranai(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        # 運勢リストの読み込み
+        with open('./csv/fortune_list.csv', 'r', encoding='utf-8') as f:
+            self.fortune_list = [s.strip() for s in f.readlines()]
+        # print(self.fortune_list)
+
+        # 運勢の星リストの初期化
+        self.star_list = ['★', '★★', '★★★', '★★★★', '★★★★★', '⭐️⭐️⭐️⭐️⭐️⭐️']
+
+        # ラッキーアイテムリストの読み込み
+        with open('./csv/lucky_list.csv', 'r', encoding='utf-8') as f:
+            self.lucky_list = [s.strip() for s in f.readlines()]
+        # print(self.lucky_list)
 
     @tasks.loop(seconds=60)
     async def loop():
@@ -63,22 +60,22 @@ class Uranai(commands.Cog):
             return
 
         # 運勢占い処理
-        random.shuffle(fortune_list)
-        logging.info('運勢 3つを決定: %s, %s, %s', fortune_list[0], fortune_list[1], fortune_list[2])
+        random.shuffle(self.fortune_list)
+        logging.info('運勢 3つを決定: %s, %s, %s', self.fortune_list[0], self.fortune_list[1], self.fortune_list[2])
 
         # 運勢用の星を算出してリストに格納
-        star_result_list = random.choices(star_list, k=3, weights=[4, 15, 50, 25, 4, 2])
+        star_result_list = random.choices(self.star_list, k=3, weights=[4, 15, 50, 25, 4, 2])
         star_result_list.sort(reverse=True)
         logging.info('運勢の星を決定: %s, %s, %s', star_result_list[0], star_result_list[1], star_result_list[2])
 
         # ラッキーアイテム占い処理
-        lucky_num = random.randint(0, len(lucky_list)-1)
-        lucky_value = lucky_list[lucky_num]
+        lucky_num = random.randint(0, len(self.lucky_list)-1)
+        lucky_value = self.lucky_list[lucky_num]
         logging.info('ラッキーアイテムを決定: %s', lucky_value)
 
         logging.info('結果を送信')
         # メッセージ送信
-        await sd.send_uranai_result(ctx, fortune_list, star_result_list, lucky_value)
+        await sd.send_uranai_result(ctx, self.fortune_list, star_result_list, lucky_value)
 
         # played_fortune_users テーブルへユーザーIDを格納する
         logging.info('played_fortune_users テーブルへ ユーザーID を格納')
