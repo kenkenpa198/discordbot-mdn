1138894413cdfb1eb90464c5771daffecb3551fa
kenkenpa198 <kenkenpa198@gmail.com>
2024-01-07 01:45:53 +0900

    [update]change id data type char to varchar

--- postgres/init/init.sh
+++ postgres/init/init.sh
@@ -9,9 +9,9 @@ psql_cmd="psql -U $POSTGRES_USER $POSTGRES_DB"
 # 読み上げチャンネルテーブルの作成
 $psql_cmd -c "\
 CREATE TABLE talk_channels (
-    guild_id   CHAR(19) NOT NULL,
-    vc_id      CHAR(19) NOT NULL,
-    channel_id CHAR(19) NOT NULL,
+    guild_id   varchar(19) NOT NULL,
+    vc_id      varchar(19) NOT NULL,
+    channel_id varchar(19) NOT NULL,
     updated_at timestamp without time zone NOT NULL,
 
     PRIMARY KEY (guild_id)
@@ -21,7 +21,7 @@ CREATE TABLE talk_channels (
 # 占い済みユーザーテーブルの作成
 $psql_cmd -c "\
 CREATE TABLE played_fortune_users (
-    user_id CHAR(19) NOT NULL,
+    user_id varchar(19) NOT NULL,
     updated_at timestamp without time zone NOT NULL,
 
     PRIMARY KEY (user_id)
